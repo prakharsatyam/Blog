9c147d4704cbcb8822c03f62361a1a35ad2a8acc
Prakhar Satyam <prakhar.2k17@gmail.com>
2024-03-27 13:34:00 +0530

    Logo Update

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -3,7 +3,7 @@
 function Logo({width='50px'}) {
   return (
     <div>
-      <img src="/public/BlogIcon.png" alt="" width={width} className="rounded-3xl border-3 border-blue-400"/>
+      <img src="https://cdn.logojoy.com/wp-content/uploads/2018/05/30164225/572.png" alt="" width={width} className="rounded-3xl border-3 border-blue-400"/>
     </div>
   )
 }
